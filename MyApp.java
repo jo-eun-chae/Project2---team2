75e1c8197ec2de5b797b0de80a1399a1fc7678e7
jo-eun-chae <dmsco9835@naver.com>
2020-09-28 13:59:13 +0900

    finish

--- MyApp.java
+++ MyApp.java
@@ -10,24 +10,35 @@ import java.util.StringTokenizer;
 public class MyApp
 {
     public static void main(String[] args){
-        MyStack<Integer> = 
+        MyStack<Integer> array = new MyStack<Integer>();
         
         System.out.println("오늘 날짜를 입력하시오(년/월/일) : ");
         Scanner scanner = new Scanner(System.in);
         String data = scanner.next();
         
         StringTokenizer stz = new StringTokenizer(data, "/");
-        
-        
-        
+
         System.out.println("------ Push ------");
-        for( int i = 0 ; i < 4 ; i++ ){
-            push[];
-            System.out.println();
+        for( int n = 0 ; n < stz.countTokens()+1 ; n++){
+            Integer Int = Integer.valueOf(stz.nextToken());
+            array.push(Int);
+            System.out.println(Int);
         }
+        
+        // for( int i = 0 ; i < 4 ; i++ ){
+            
+            // array.push();
+        // }
+        
         System.out.println("------ Pop ------");
         for( int j = 0 ; j < 5 ; j++){
-            pop();
+            try{
+                System.out.println(array.pop());
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("Stsck underflow!");
+                System.out.println("null");
+            }
         }
     }
 }
